c23ef8ffe4aaf6ce8cae5712e36ef8101c89b689
ray omego <ray.omego@student.moringaschool.com>
2022-08-30 09:17:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(nameArg, emailArg) {
+
+    const formData = {
+
+        name: nameArg,
+        email: emailArg,
+
+    }
+
+    const configObject = {
+
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+    return fetch('http://localhost:3000/users', configObject)
+            .then(response => response.json())
+            .then(data => document.body.append(data.id))
+            .catch(error => document.body.append(error));
+} 
